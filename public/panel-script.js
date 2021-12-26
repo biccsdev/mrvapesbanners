2563833e50ae844937fc3b319596c63fd8b67097
biccs <biccsdev@gmail.com>
2021-12-25 22:23:17 -0700

    panel de control finished, displaying the images on inicio left

--- public/panel-script.js
+++ public/panel-script.js
@@ -1,18 +1,32 @@
-const upload = document.getElementById("upload");
-const image = document.getElementById("image");
-const title = document.getElementById("name");
-const fileName = document.getElementById("file-name");
-const deleteButton = document.getElementById("delete-button");
 const list = document.getElementById("files-container");
+const filesForm = document.getElementById("files-form");
 
 fetch('http://localhost:3000/banner')
     .then(response => response.json())
     .then(json => {
         console.log(json)
         console.log(json.message)
-            // temp = json.message;\
         for (let i = 0; i < json.message.length; i++) {
-            var item = `<li>${json.message[i].name}<button id="delete-button">Borrar</button></li>`;
+            var item = `<li>${json.message[i].name}
+                            <button type="submit" id="delete-button" onclick="deleteButton('${json.message[i].name}','${json.message[i]._id}')">Borrar</button>
+                        </li>`;
             list.innerHTML += item;
         }
     });
+
+function deleteButton(name, id) {
+    var answer = confirm(`Seguro que deseas borrar ${name}?`);
+    console.log(answer);
+    if (answer) {
+        filesForm.onsubmit = "return true;"
+        filesForm.action = `http://localhost:3000/banner/${id}`;
+        alert('Borrado Correctamente!');
+        window.location.reload(true);
+    } else {
+        validate();
+    }
+}
+
+function validate() {
+    return false
+}
